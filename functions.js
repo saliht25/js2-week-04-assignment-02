0688c07c2e5b883ba0a5d5883de9f1f67da812f0
saliht25 <107279476+saliht25@users.noreply.github.com>
2022-12-08 15:04:54 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,14 @@
-const isTheWordSplit = () => {};
+const isTheWordSplit = (pArray) => {
+    let word = pArray[0];
+    let array = pArray[1].split(",");
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] + array[j] == word) {
+                return array[i] + "," + array[j];
+            }
+        }
+    }
+    return "not possible";
+};
 
 export { isTheWordSplit };
